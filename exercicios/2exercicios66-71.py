9d63e0b2340a25eb0547cc8a62ae0b582614d3bc
Matheus <mat.araujoribeiro@gmail.com>
2023-11-17 15:07:36 -0300

    [add] challenge 66

---
+++ exercicios/2exercicios66-71.py
@@ -0,0 +1,37 @@
+import os
+
+os.system('cls' if os.name == 'nt' else 'clear') # limpar console
+
+# 66. mostrar soma dos números e contador. Condição de parada = 999
+def somaEContador():
+    soma = contador = 0
+    num = int(input('Digite um nº: '))
+
+    if num == 999:
+        print('Vc saiu do programa sem fazer soma alguma.')
+    else:
+        soma += num
+        contador += 1
+
+        while num != 999:
+            num = int(input('Digite um nº: '))
+            if num == 999:
+                break
+            soma += num
+            contador += 1
+
+    print(f'A soma deu {soma} e a quantidade de nºs foi de {contador}')
+# somaEContador()
+
+def somaEContador2():
+    soma = count = num = 0
+
+    while True:
+        num = int(input('Digite um nº: '))
+        if num == 999:
+            break
+        soma += num
+        count += 1
+    print(f'A soma é {soma} e a qntidade de nºs usados foi {count}')
+# somaEContador2()
+
