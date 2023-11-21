b0a8d8cd513ccbc25f923b61366ac227711317c0
Matheus <mat.araujoribeiro@gmail.com>
2023-11-21 13:12:41 -0300

    [add] dic lesso

---
+++ aulas/m3Dicionarios19.py
@@ -0,0 +1,23 @@
+# DICIONÁRIOS
+# São identificados por chaves
+
+dados = {'Nome': 'Pedro',
+         'Idade': 25
+         }
+# print(dados['nome'])
+# print(dados['idade'])
+# dados['sexo'] = 'M'
+# print(dados['sexo'])
+# del dados['idade']
+#
+# print(dados.values())
+# print(dados.keys())
+t = dados.get('nome')
+# print(t)
+# print(dados.items())
+# print(dados)
+#
+# for k, v in dados.items():
+#     print(f'{k} é {v}')
+
+#PRática
